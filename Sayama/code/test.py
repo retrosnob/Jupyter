8d0d93b8853b99edc8c930cf39032d772a275d21
retrosnob <jusrobertson@yahoo.co.uk>
2019-03-01 10:07:15 +0800

    Adding predator-prey content

--- Sayama/code/test.py
+++ Sayama/code/test.py
@@ -1,24 +1,12 @@
-import matplotlib.pylab as pylab
+import matplotlib.pylab as plt
 
-a = 1.1
+fig1 = plt.figure()
+fig2 = plt.figure()
 
-def initialize():
-    global x, result
-    x = 1
-    result = [x]
+ax1 = fig1.add_subplot(111)
+ax2 = fig2.add_subplot(111)
 
-def observe():
-    global x, result
-    result.append(x)
+ax1.plot([1, 2, 3],[1, 2, 3])
+ax2.plot([1, 2, 3],[1, 2, 3])
 
-def update():
-    global x, result
-    x = a * x
-
-initialize()
-for t in range(30):
-    update()
-    observe()
-
-pylab.plot(result)
-pylab.show()
+plt.show()
